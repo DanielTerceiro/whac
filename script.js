f64ea99a17bbc77b5c6a40e9d4b67a760bb49f48
DanielTerceiro <daniel_mt554@hotmail.com>
2021-11-25 18:05:30 +0000

    Add all -let- elements

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const startButton = document.querySelector('.startButton')
 const highscore = document.querySelector('.highscore')
 const cursor = document.querySelector('.cursor img')
 const icon = document.querySelector('.icon')
+
+let lastHole
+let timeUp = false
+let timeLimit = 20000
+let score = 0
+let countdown
